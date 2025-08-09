c57ba597993433479c60bca25c499b840beec368
jmachado11 <jm811machado@gmail.com>
2025-08-08 23:50:46 -0400

    speed insights

--- src/main.tsx
+++ src/main.tsx
@@ -3,5 +3,7 @@ import App from './App.tsx'
 import './index.css'
 import { inject } from '@vercel/analytics';
 inject();
+import { injectSpeedInsights } from '@vercel/speed-insights';
+injectSpeedInsights();
 
 createRoot(document.getElementById("root")!).render(<App />);
